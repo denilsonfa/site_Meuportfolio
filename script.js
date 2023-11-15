7d1516a59b6042cd39aae5851efccba39a5031e9
Denilson Araujo <dlnilso147@gmail.com>
2023-11-15 19:45:58 -0300

    update 1115

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  x.classList.toggle("responsive", x.classList.contains("topnav"));
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
 // Mudança de categorias
